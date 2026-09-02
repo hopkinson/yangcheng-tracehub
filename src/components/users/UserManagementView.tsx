2101fcc8b6610330f36809f9986610accb6d5299
何大小成 <1651690426@qq.com>
2026-09-02 16:01:59 +0800

    fix(users): add 'use client' directive to UserManagementView

--- src/components/users/UserManagementView.tsx
+++ src/components/users/UserManagementView.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from "react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Badge } from "@/components/ui/badge";
